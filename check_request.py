91b2e94d88859f9313f0dfb2f1bd24cece25b43c
wepeqoor1 <hoopeeeee@gmail.com>
2023-01-29 22:56:43 +0300

    Add search new requests with long polling

--- check_request.py
+++ check_request.py
@@ -1,19 +1,33 @@
 import sys
+from datetime import datetime
 
 import requests  # type: ignore
 from loguru import logger
 from environs import Env
 
 
-def get_list_of_checks(devman_api_token: str) -> dict:
-    url = 'https://dvmn.org/api/user_reviews/'
+def get_list_of_checks(devman_api_token: str, timeout: int = 300) -> None:
+    timestamp = datetime.now().timestamp()
+    logger.debug(f'TIMESTAMP_NOW: {timestamp}')
+
     headers = {
         'Authorization': f'Token {devman_api_token}'
     }
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
+    while True:
+        url = f'https://dvmn.org/api/long_polling/?timestamp={timestamp}'
+        logger.debug(url)
+        logger.info(f'Запустили LONG POLLING. Таймаут {timeout} секунд')
+        try:
+            response = requests.get(url, headers=headers, timeout=timeout)
+            response.raise_for_status()
+            response_json = response.json()
 
-    return response.json()
+            timestamp = response_json.get('timestamp_to_request') or response_json.get('last_attempt_timestamp')
+            logger.info(response.json())
+        except requests.exceptions.ReadTimeout as error:
+            logger.warning(f'Таймаут запроса отработал раньше чем сервер ответил: {error}')
+            timestamp = datetime.now().timestamp()
+            continue
 
 
 def main():
@@ -25,7 +39,7 @@ def main():
     logger_level = "DEBUG" if env('DEBUG_MODE', False) else "INFO"
     logger.add(sys.stderr, format="{time} {level} {message}", level=logger_level)
 
-    list_of_checks = get_list_of_checks(devman_api_token)
+    get_list_of_checks(devman_api_token, 5)
 
 
 if __name__ == '__main__':
