2bfcf8281e6438b8a716d62bbeadfc7c6c4b68c3
wepeqoor1 <hoopeeeee@gmail.com>
2023-01-31 14:14:32 +0300

    Join functions

--- check_request.py
+++ check_request.py
@@ -1,3 +1,4 @@
+import sys
 import time
 from datetime import datetime
 from textwrap import dedent
@@ -7,19 +8,6 @@ import telegram
 from environs import Env
 from loguru import logger
 from telegram import ParseMode
-
-
-def get_response(url: str, headers: dict = None, params: dict = None, timeout: int = None) -> requests.Response:
-    reconnect_time = 0.1
-    while True:
-        try:
-            response = requests.get(url, headers=headers, params=params, timeout=timeout)
-            response.raise_for_status()
-            return response
-        except requests.exceptions.ConnectionError:
-            time.sleep(reconnect_time)
-            reconnect_time *= 2
-            logger.warning(f'Потеря соединения. Повторный запрос через {reconnect_time} секунд')
 
 
 def send_telegram_message(response_json: dict, chat_id: int, bot: telegram.Bot) -> None:
@@ -44,28 +32,37 @@ def get_new_checks(devman_api_token: str, bot: telegram.Bot, chat_id: int, timeo
     headers = {'Authorization': f'Token {devman_api_token}'}
     params = {'timestamp': timestamp}
 
+    reconnect_time = 0.1
+
     while True:
         url = f'https://dvmn.org/api/long_polling/'
         logger.info(f'Запустили LONG POLLING. Таймаут {timeout} секунд')
         try:
             # Делаем запрос к API
-            response = get_response(url, headers=headers, params=params, timeout=timeout)
-            response_api = response.json()
-            logger.debug(response_api)
-            # Если запрос верный - отправляем сообщение в Телеграм
-            if response_api.get('status') == 'found':
-                send_telegram_message(response_api, chat_id, bot)
+            response = requests.get(url, headers=headers, params=params, timeout=timeout)
+            response.raise_for_status()
 
-            timestamp = response_api.get('timestamp_to_request') or response_api.get('last_attempt_timestamp')
+            checked_tasks = response.json()
+            logger.debug(checked_tasks)
+            # Если запрос верный - отправляем сообщение в Телеграм
+            if checked_tasks.get('status') == 'found':
+                send_telegram_message(checked_tasks, chat_id, bot)
+
+            timestamp = checked_tasks.get('timestamp_to_request') or checked_tasks.get('last_attempt_timestamp')
             params = {'timestamp': timestamp}
-            logger.info(response_api)
+            reconnect_time = 0.1
+            logger.info(checked_tasks)
 
         except requests.exceptions.ReadTimeout as error:
             logger.warning(f'Таймаут запроса отработал раньше чем сервер ответил: {error}')
             timestamp = datetime.now().timestamp()
             params = {'timestamp': timestamp}
-
             continue
+
+        except requests.exceptions.ConnectionError:
+            time.sleep(reconnect_time)
+            reconnect_time *= 2
+            logger.warning(f'Потеря соединения. Повторный запрос через {reconnect_time} секунд')
 
 
 def main():
@@ -79,7 +76,7 @@ def main():
     bot = telegram.Bot(token=telegram_api_key)
 
     logger_level = 'DEBUG' if env('DEBUG_MODE', False) else 'INFO'
-    logger.add('logging.log', format='{time} {level} {message}', level=logger_level)
+    logger.add(sys.stdout, format='{time} {level} {message}', level=logger_level)
 
     get_new_checks(devman_api_token, bot, telegram_chat_id)
 
